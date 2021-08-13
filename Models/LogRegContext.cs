924741fdb5ec1ed9f09d7955eeeee485cd092ca1
Lawrence <45595421+Zirkander@users.noreply.github.com>
2021-08-12 23:28:50 -0700

    Created the login and register views and partials

--- Models/LogRegContext.cs
+++ Models/LogRegContext.cs
@@ -2,8 +2,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LogReg.Models
 {
-    public class LogRegContext
+    public class LogRegContext : DbContext
     {
+        public LogRegContext(DbContextOptions options) : base(options) { }
 
+        // public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace LogReg
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<LogRegContext>(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
-            services.AddMvc(option => OptionsBuilderConfigurationExtensions.EnableEndpointRouting = false);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
